f8487d2f199dd1ab0bb9349b6ab9399b9b558584
lingxiao106 <1060369102@qq.com>
2019-04-28 14:44:46 +0800

    123

--- src/components/page/Answer.vue
+++ src/components/page/Answer.vue
@@ -1,31 +1,73 @@
 <template>
   <el-row>
-    <el-col :span="5" :xs="20" v-for="(o, index) in 8" :key="o" style="margin-top: 20px;margin-left: 30px;margin-right: 26px">
+    <el-col :span="5" :xs="20" v-for="(o, index) in list" :key="o"
+            style="margin-top: 20px;margin-left: 30px;margin-right: 26px">
       <el-card :body-style="{ padding: '0px' }">
-        <img src="https://test-1256499519.cos.ap-guangzhou.myqcloud.com/jso3nvyj.jpg" height="200px" width="200px"
-             class="image">
+        <img
+          :src="o.image" height="200px" width="200px"
+          class="image" style="margin-top: 20px;border-radius: 30px;"
+          @click="answerInfo(o.id)">
         <div style="padding: 14px;">
-          <span>好吃的汉堡</span>
+          <span>{{o.title}}</span>
           <div class="bottom clearfix">
             <time class="time"></time>
-            <el-button type="text" class="button">查看详情</el-button>
+            <el-button type="text" class="button" @click="answerInfo(o.id)">查看详情</el-button>
           </div>
         </div>
       </el-card>
+    </el-col>
+    <el-col v-if="list.size > 10">
+      <div :span="24">
+        <el-pagination background
+                       @current-change="handleCurrentChange"
+                       :current-page="currentPage"
+                       :page-size="pagesize"
+                       layout="total, prev, pager, next, jumper"
+                       :total="total">
+        </el-pagination>
+      </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
   export default {
+    name: 'answer',
     data () {
       return {
-        answerList: null
+        loading: true,
+        list: [],
+        total: null,
+        currentPage: 1,
+        pagesize: 10
       }
     },
-    methods: {},
-    mounted: function () {
-
+    methods: {
+      answerInfo (id) {
+        this.$router.push({name: 'answerInfo', params: {id: id}})
+      },
+      load () {
+        let a = this
+        this.loading = true
+        this.axios.get('http://192.168.1.175:8089/' + 'api/answer/getAnswerInfo', {
+          params: {
+            pageNum: this.currentPage,
+            pageSize: this.pagesize,
+          }
+        }).then(response => {
+          this.list = response.data.data.data
+          this.total = response.data.data.total
+        }).catch(function (error) {
+          a.$message.error('系统出现异常请联系管理人员！')
+        }).finally(() => this.loading = false)
+      },
+      handleCurrentChange (val) {
+        this.currentPage = val
+        this.load()
+      }
+    },
+    created () {
+      this.load()
     }
   }
 </script>
